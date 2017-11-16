af2cf2712e76c45ee452ed6c032213034ab55840
ronaldoemac <ronaldoemac@gmail.com>
2017-11-15 23:44:16 -0200

    Criamos métodos de lista

--- index.php
+++ index.php
@@ -8,11 +8,23 @@ require_once("config.php");
 
 //echo json_encode($usuarios);
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo Login
+//$search = Usuario::search("o");
+//echo json_encode($search);
 
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "*NewPass");
+
+echo $usuario;
 
 ?>
